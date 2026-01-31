550fe0374965dfbc050b2f31e46e40056c45744a
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2026-01-31 23:27:02 +0530

    SEU_IS_20_ICT_037: Update UI design of Main Home Page and User Profile Page

--- flutter_project/lib/main_home_page.dart
+++ flutter_project/lib/main_home_page.dart
@@ -40,7 +40,7 @@ class _MainHomePageState extends State<MainHomePage> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined), label: 'Shop'),
+              icon: Icon(Icons.shop), label: 'Category'),
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite_border), label: 'Favorites'),
           BottomNavigationBarItem(
